fc45fd08bdb6faf36b65a296bd9c866d394caa6f
CharlieLydia <99601638+CharlieLydia@users.noreply.github.com>
2024-02-01 16:54:28 +0000

    Add files via upload

---
+++ src/src/HomePageComponents/UploadImage.js
@@ -0,0 +1,16 @@
+import React from "react";
+import './HomePage.css';
+
+export default function ProfilePic(){
+//function handleChange() {}
+//function handleClick(){}
+//<input type="file" onChange={handleChange} />
+//<button onClck={handleClick}> Upload </button>
+
+
+    return(
+    <div className="pic">
+    <img src="./avatar.png" alt ="Avatar" className="avatar" />
+    </div>
+);
+}
